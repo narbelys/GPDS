f30e56b4f505d3757f617eaea679bc0e54aea4b2
Vicente Santacoloma <vsantacoloma@gmail.com>
2012-06-08 11:03:08 -0430

    Se modifico agrego enlazes a las opciones del base.html

--- GPDS/views.py
+++ GPDS/views.py
@@ -6,3 +6,21 @@ from django.contrib.auth.models import User
 
 def home(request):
     return render_to_response('home.html', {} , context_instance=RequestContext(request))
+
+def version(request):
+    return render_to_response('version.html', {} , context_instance=RequestContext(request))
+
+def authors(request):
+    return render_to_response('authors.html', {} , context_instance=RequestContext(request))
+
+def license(request):
+    return render_to_response('license.html', {} , context_instance=RequestContext(request))
+
+def tutorial(request):
+    return render_to_response('tutorial.html', {} , context_instance=RequestContext(request))
+
+def support(request):
+    return render_to_response('support.html', {} , context_instance=RequestContext(request))
+
+def help(request):
+    return render_to_response('help.html', {} , context_instance=RequestContext(request))
