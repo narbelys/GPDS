c52a446969470b7ae8b0f60e9c52196be067749e
Fernando Sahmkow <fernandosahmkow1@hotmail.com>
2012-06-07 21:57:54 -0430

    Agreagdo listar actividad

--- activity/urls.py
+++ activity/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     # Technique Details
     url(r'^indextec/(?P<technique_id>\d+)/$', 'activity.views.detailtec'),
     url(r'^(?P<activity_id>\d+)/artifacts/(?P<artifact_id>\d+)$', 'activity.views.open_artifact'), 
-    url(r'^(?P<activity_id>\d+)/artifacts$', 'activity.views.artifacts'),  
+    url(r'^(?P<activity_id>\d+)/artifacts$', 'activity.views.artifacts'), 
 )
